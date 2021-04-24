07d089c556217ff7a8ba783121ae89f5b460f137
BrunoUemura <bruno.h.uemura@outlook.com>
2021-04-24 00:43:37 -0300

    added views

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import path from "path";
 
 import "./database";
 import { routes } from "./routes";
@@ -6,8 +9,24 @@ import { routes } from "./routes";
 const app = express();
 const PORT = 3333;
 
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.set("views", path.join(__dirname, "..", "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get("/pages/client", (request, response) => {
+  return response.render("html/client.html");
+});
+
+const http = createServer(app); // Criando protocolo http
+const io = new Server(http); // Criando protocolo ws
+
+io.on("connection", (socket: Socket) => {
+  console.log("Se conectou", socket.id);
+});
+
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
