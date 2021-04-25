2c93e78a5174ec404b119f3172479b38aafed543
BrunoUemura <bruno.h.uemura@outlook.com>
2021-04-25 13:00:03 -0300

    created connections migration

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,27 @@
 document.querySelector("#start_chat").addEventListener("click", (event) => {
-  console.log("clicou no botão");
+  const socket = io();
+
+  const chat_help = document.querySelector("#chat_help");
+  chat_help.style.display = "none";
+
+  const chat_in_support = document.querySelector("#chat_in_support");
+  chat_in_support.style.display = "block";
+
+  const email = document.querySelector("#email").value;
+  const text = document.querySelector("#txt_help").value;
+
+  socket.on("connect", () => {
+    const params = {
+      email,
+      text,
+    };
+
+    socket.emit("client_first_access", params, (call, err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(call);
+    });
+  });
 });
--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,6 @@
-import express from "express";
-import { createServer } from "http";
-import { Server, Socket } from "socket.io";
-import path from "path";
+import { http } from "./http";
+import "./websocket/client";
 
-import "./database";
-import { routes } from "./routes";
-
-const app = express();
 const PORT = 3333;
-
-app.use(express.static(path.join(__dirname, "..", "public")));
-app.set("views", path.join(__dirname, "..", "public"));
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "html");
-
-app.get("/pages/client", (request, response) => {
-  return response.render("html/client.html");
-});
-
-const http = createServer(app); // Criando protocolo http
-const io = new Server(http); // Criando protocolo ws
-
-io.on("connection", (socket: Socket) => {
-  console.log("Se conectou", socket.id);
-});
-
-app.use(express.json());
-
-app.use(routes);
 
 http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
